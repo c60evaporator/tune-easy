20e627749642c7de8e4b2cf69447944f2866c4dd
c60evaporator <c60evaporator@gmail.com>
2021-08-23 01:23:43 +0900

    Add userguide_examples

---
+++ examples/examples_in_readme/userguide_examples.py
@@ -0,0 +1,149 @@
+# %% 0.1.1. Feature selection
+from sklearn.datasets import load_boston
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+
+X_all = load_boston().data  # Feature values
+y = load_boston().target  # Objective variable
+selector = RFE(RandomForestRegressor(random_state=42), n_features_to_select=5)
+selector.fit(X_all, y)
+print(load_boston().feature_names)
+print(selector.get_support())
+
+# %% 0.1.2. Load dataset with selected explanatory variables
+from sklearn.datasets import load_boston
+import pandas as pd
+
+USE_EXPLANATORY = ['CRIM', 'NOX', 'RM', 'DIS', 'LSTAT']  # Selected explanatory variables
+df_boston = pd.DataFrame(load_boston().data, columns = load_boston().feature_names)
+X = df_boston[USE_EXPLANATORY].values  # Explanatory variables
+y = load_boston().target  # Objective variable
+
+# %% 0.2. Confirm validation score before tuning
+
+
+# %% 0.3. Initialize tuning class
+import parent_import
+from param_tuning import LGBMRegressorTuning
+tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)
+
+# %% 1. Select validation score
+SCORING = 'neg_mean_squared_error'  # RMSE
+
+# %% 2. Select parameter range using validation curve
+from sklearn.model_selection import KFold
+# Parameter range for validation curve
+VALIDATION_CURVE_PARAMS = {'reg_alpha': [0, 0.0001, 0.001, 0.003, 0.01, 0.03, 0.1, 1, 10],
+                           'reg_lambda': [0, 0.0001, 0.001, 0.003, 0.01, 0.03, 0.1, 1, 10],
+                           'num_leaves': [2, 4, 8, 16, 32, 64, 96, 128, 192, 256],
+                           'colsample_bytree': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                           'subsample': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                           'subsample_freq': [0, 1, 2, 3, 4, 5, 6, 7],
+                           'min_child_samples': [0, 2, 5, 10, 20, 30, 50, 70, 100]
+                           }
+# Plot validation curve
+tuning.plot_first_validation_curve(validation_curve_params=VALIDATION_CURVE_PARAMS,
+                                   scoring=SCORING,
+                                   cv=KFold(n_splits=5, shuffle=True, random_state=42)
+                                   )
+
+# %% 4.1 Select cross validation instance
+CV = KFold(n_splits=5, shuffle=True, random_state=42)
+
+# %% 4.2.1 Calculate validation score before tuning
+from lightgbm import LGBMRegressor
+from sklearn.model_selection import cross_val_score
+import numpy as np
+# Fit parameters passed to estimator.fit()
+FIT_PARAMS = {'verbose': 0,
+              'early_stopping_rounds': 10,
+              'eval_metric': 'rmse',
+              'eval_set': [(X, y)]
+              }
+# Parameters not used in optimization
+NOT_OPT_PARAMS = {'objective': 'regression',
+                  'random_state': 42,
+                  'boosting_type': 'gbdt',
+                  'n_estimators': 10000
+                  }
+# Make estimator instance
+lgbmr = LGBMRegressor(**NOT_OPT_PARAMS)
+# Calculate validation score
+scores = cross_val_score(lgbmr, X, y,
+                         scoring=SCORING,  # Validation score selected in section 1
+                         cv=CV,  # Cross validation instance selected in section 4.2
+                         fit_params=FIT_PARAMS  # Fit parameters passed to estimator.fit()
+                         )
+print(np.mean(scores))
+
+# %% 4.2.2 Visualize estimator before tuning
+from seaborn_analyzer import regplot
+df_boston['price'] = y
+regplot.regression_pred_true(lgbmr,
+                             x=tuning.x_colnames,
+                             y='price',
+                             data=df_boston,
+                             scores='mse',
+                             cv=CV,
+                             fit_params=FIT_PARAMS
+                             )
+
+# %% 4.3 Execute parameter tuning
+# Select parameter range of optuna
+TUNING_PARAMS = {'reg_alpha': (0.0001, 0.1),
+                 'reg_lambda': (0.0001, 0.1),
+                 'num_leaves': (2, 50),
+                 'colsample_bytree': (0.4, 1.0),
+                 'subsample': (0.4, 1.0),
+                 'subsample_freq': (0, 7),
+                 'min_child_samples': (0, 50)
+                 }
+# Execute parameter tuning
+best_params, best_score = tuning.optuna_tuning(scoring=SCORING,
+                                               tuning_params=TUNING_PARAMS,
+                                               cv=CV,
+                                               not_opt_params=NOT_OPT_PARAMS,
+                                               fit_params=FIT_PARAMS
+                                               )
+print(f'Best parameters\n{best_params}\n')  # Optimized parameters
+print(f'Not tuned parameters\n{tuning.not_opt_params}\n')  # Parameters not used in optimization
+print(f'Best score\n{best_score}\n')  # Best score in optimized parameters
+print(f'Elapsed time\n{tuning.elapsed_time}\n')  # Elapsed time
+
+# %% 5.1 Plot search history
+tuning.plot_search_history()
+
+# %% 5.2 Visualize relationship between parameters and validation score
+tuning.plot_search_map()
+
+# %% 5.3 Plot learning curve
+tuning.plot_best_learning_curve()
+
+# %% 5.4 Plot validation curve
+tuning.plot_best_validation_curve()
+
+# %% 6.1 Calculate validation score after tuning
+params_after = {}
+params_after.update(tuning.best_params)
+params_after.update(tuning.not_opt_params)
+lgbmr = LGBMRegressor(**params_after)
+# Calculate validation score
+scores = cross_val_score(lgbmr, X, y,
+                         scoring=tuning.scoring,  # Validation score selected in section 1
+                         cv=tuning.cv,  # Cross validation instance selected in section 4.2
+                         fit_params=tuning.fit_params  # Fit parameters passed to estimator.fit()
+                         )
+print(np.mean(scores))
+
+# %% 6.2 Visualize estimator after tuning
+from seaborn_analyzer import regplot
+df_boston['price'] = y
+regplot.regression_pred_true(lgbmr,
+                             x=tuning.x_colnames,
+                             y='price',
+                             data=df_boston,
+                             scores='mse',
+                             cv=tuning.cv,
+                             fit_params=tuning.fit_params
+                             )
+# %%
