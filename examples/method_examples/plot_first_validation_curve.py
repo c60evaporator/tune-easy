5d39f09ff26ee723ce3bdf4396e02458d1767e10
c60evaporator <c60evaporator@gmail.com>
2021-08-25 01:10:02 +0900

    Update examples of validation_curve_params()

---
+++ examples/method_examples/plot_first_validation_curve.py
@@ -0,0 +1,36 @@
+# %% Plot_first_validation_curve(), no argument
+import parent_import
+from param_tuning import LGBMRegressorTuning
+from sklearn.datasets import load_boston
+import pandas as pd
+# Load dataset
+USE_EXPLANATORY = ['CRIM', 'NOX', 'RM', 'DIS', 'LSTAT']
+df_boston = pd.DataFrame(load_boston().data, columns = load_boston().feature_names)
+X = df_boston[USE_EXPLANATORY].values
+y = load_boston().target
+tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)
+###### Run plot_first_validation_curve() ######
+tuning.plot_first_validation_curve()
+
+# %% plot_first_validation_curve(), Set parameter range by 'validation_curve_params' argument
+import parent_import
+from param_tuning import LGBMRegressorTuning
+from sklearn.datasets import load_boston
+import pandas as pd
+# Load dataset
+USE_EXPLANATORY = ['CRIM', 'NOX', 'RM', 'DIS', 'LSTAT']
+df_boston = pd.DataFrame(load_boston().data, columns = load_boston().feature_names)
+X = df_boston[USE_EXPLANATORY].values
+y = load_boston().target
+tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)
+# Give 'validation_curve_params' argument
+VALIDATION_CURVE_PARAMS = {'reg_lambda': [0.0001, 0.001, 0.01, 0.1, 1, 10],
+                           'num_leaves': [2, 4, 8, 16, 32, 64],
+                           'colsample_bytree': [0.2, 0.4, 0.6, 0.8, 1.0],
+                           'subsample': [0.2, 0.4, 0.6, 0.8, 1.0],
+                           'min_child_samples': [0, 5, 10, 20, 30, 50]
+                           }
+###### plot_first_validation_curve() ######
+tuning.plot_first_validation_curve(validation_curve_params=VALIDATION_CURVE_PARAMS)
+
+# %%
---
+++ examples/usage_examples/usage_examples.py
@@ -0,0 +1,18 @@
+# %% Usage
+import parent_import
+from param_tuning import LGBMRegressorTuning
+from sklearn.datasets import load_boston
+import pandas as pd
+# データセット読込
+USE_EXPLANATORY = ['CRIM', 'NOX', 'RM', 'DIS', 'LSTAT']  # 説明変数
+boston = pd.DataFrame(load_boston().data, columns = load_boston().feature_names)
+X = boston[USE_EXPLANATORY].values
+y = load_boston().target
+###### チューニング実行と評価 ######
+tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)  # チューニング用クラス
+tuning.plot_first_validation_curve()  # 範囲を定めて検証曲線をプロット
+tuning.optuna_tuning()  # Optunaによるチューニング実行
+tuning.plot_search_history()  # Optuna実行
+tuning.plot_search_map()  # 探索点と評価指標を可視化
+tuning.plot_best_learning_curve()  # 学習曲線の可視化
+tuning.plot_best_validation_curve()  # 検証曲線の可視化
