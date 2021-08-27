2a1ddb1edfb5e34b3ba27a127b7a8fe36e0aba38
c60evaporator <c60evaporator@gmail.com>
2021-08-28 02:54:35 +0900

    Modify plot_search_map.py

--- examples/method_examples/plot_search_map.py
+++ examples/method_examples/plot_search_map.py
@@ -12,4 +12,20 @@ tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)
 best_params, best_score = tuning.optuna_tuning()
 ###### Run plot_search_map() ######
 tuning.plot_search_map()
+
+# %% plot_search_map(), set number of maps by 'pair_n' argument, set axis parameter by 'order' argument
+import parent_import
+from param_tuning import LGBMRegressorTuning
+from sklearn.datasets import load_boston
+import pandas as pd
+# Load dataset
+USE_EXPLANATORY = ['CRIM', 'NOX', 'RM', 'DIS', 'LSTAT']
+df_boston = pd.DataFrame(load_boston().data, columns=load_boston().feature_names)
+X = df_boston[USE_EXPLANATORY].values
+y = load_boston().target
+tuning = LGBMRegressorTuning(X, y, USE_EXPLANATORY)
+best_params, best_score = tuning.optuna_tuning()
+###### Run plot_search_map() ######
+tuning.plot_search_map(pair_n=6, 
+                       order=['min_child_samples', 'colsample_bytree', 'subsample', 'num_leaves'])
 # %%
