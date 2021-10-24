dbb022539772c07ca2566aa114f8b4700e79d751
c60evaporator <c60evaporator@gmail.com>
2021-10-24 20:46:20 +0900

    Modify `eval_set ` generation in pipeline

--- muscle_tuning/lgbm_tuning.py
+++ muscle_tuning/lgbm_tuning.py
@@ -105,15 +105,16 @@ class LGBMRegressorTuning(ParamTuning):
             # src_fit_paramsにeval_setが存在しないとき、入力データをそのまま追加
             if 'eval_set' not in src_fit_params:
                 print('There is no "eval_set" in fit_params, so "eval_set" is set to (self.X, self.y)')
-                # estimatorがパイプラインのとき、eval_setに最終学習器以外のtransformを適用
-                if isinstance(estimator, Pipeline):
-                    print('The estimator is Pipeline, so X data of "eval_set" is transformed using pipeline')
-                    X_src = self.X
-                    transformer = Pipeline([step for i, step in enumerate(estimator.steps) if i < len(estimator) - 1])
-                    X_dst = transformer.fit_transform(X_src)
-                else:
-                    X_dst = self.X
-                src_fit_params['eval_set'] =[(X_dst, self.y)]
+                src_fit_params['eval_set'] = [(self.X, self.y)]
+            # estimatorがパイプラインかつeval_data_sourceが'original_transferred'以外のとき、eval_setに最終学習器以外のtransformを適用
+            if isinstance(estimator, Pipeline) and self.eval_data_source != 'original_transferred':
+                print('The estimator is Pipeline, so X data of "eval_set" is transformed using pipeline')
+                X_src = self.X
+                transformer = Pipeline([step for i, step in enumerate(estimator.steps) if i < len(estimator) - 1])
+                X_dst = transformer.fit_transform(X_src)
+            else:
+                X_dst = self.X
+            src_fit_params['eval_set'] = [(X_dst, self.y)]
 
         return src_fit_params
 
@@ -271,15 +272,16 @@ class LGBMClassifierTuning(ParamTuning):
             # src_fit_paramsにeval_setが存在しないとき、入力データをそのまま追加
             if 'eval_set' not in src_fit_params:
                 print('There is no "eval_set" in fit_params, so "eval_set" is set to (self.X, self.y)')
-                # estimatorがパイプラインのとき、eval_setに最終学習器以外のtransformを適用
-                if isinstance(estimator, Pipeline):
-                    print('The estimator is Pipeline, so X data of "eval_set" is transformed using pipeline')
-                    X_src = self.X
-                    transformer = Pipeline([step for i, step in enumerate(estimator.steps) if i < len(estimator) - 1])
-                    X_dst = transformer.fit_transform(X_src)
-                else:
-                    X_dst = self.X
-                src_fit_params['eval_set'] =[(X_dst, self.y)]
+                src_fit_params['eval_set'] = [(self.X, self.y)]
+            # estimatorがパイプラインかつeval_data_sourceが'original_transferred'以外のとき、eval_setに最終学習器以外のtransformを適用
+            if isinstance(estimator, Pipeline) and self.eval_data_source != 'original_transferred':
+                print('The estimator is Pipeline, so X data of "eval_set" is transformed using pipeline')
+                X_src = self.X
+                transformer = Pipeline([step for i, step in enumerate(estimator.steps) if i < len(estimator) - 1])
+                X_dst = transformer.fit_transform(X_src)
+            else:
+                X_dst = self.X
+            src_fit_params['eval_set'] = [(X_dst, self.y)]
 
             # 2クラス分類のときeval_metricはbinary_logloss or binary_errorを、多クラス分類のときmulti_logloss or multi_errorを入力
             unique_labels = np.unique(self.y)
