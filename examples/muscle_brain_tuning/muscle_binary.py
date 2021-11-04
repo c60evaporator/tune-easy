3ad4851f0dba24d4ff176b734a589f9e5c25ba4d
knakamura <c60evaporator@gmail.com>
2021-11-05 01:55:44 +0900

    Modify example of muscle_tuning binary

--- examples/muscle_brain_tuning/muscle_binary.py
+++ examples/muscle_brain_tuning/muscle_binary.py
@@ -2,16 +2,16 @@
 import parent_import
 from muscle_tuning import MuscleTuning
 import seaborn as sns
-
+# Load dataset
 iris = sns.load_dataset("iris")
-iris['species'] = iris['species'].map(lambda x: x.replace('virginica', 'setosa'))
+iris['species'] = iris['species'].map(lambda x: x.replace('versicolor', 'setosa'))
 OBJECTIVE_VARIALBLE = 'species'  # 目的変数
 USE_EXPLANATORY = ['petal_width', 'petal_length', 'sepal_width', 'sepal_length']  # 説明変数
 y = iris[OBJECTIVE_VARIALBLE].values
 X = iris[USE_EXPLANATORY].values
-
+# Run parameter tuning
 kinnikun = MuscleTuning()
-kinnikun.muscle_brain_tuning(X, y, x_colnames=USE_EXPLANATORY)
+kinnikun.muscle_brain_tuning(X, y, x_colnames=USE_EXPLANATORY, cv=2)
 kinnikun.df_scores
 
 # %%
