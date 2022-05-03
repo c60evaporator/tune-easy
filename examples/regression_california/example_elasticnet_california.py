368e7abdc162201ece13ffdc30cc8605b3567dc5
knakamura <c60evaporator@gmail.com>
2022-05-03 22:18:51 +0900

    Small modification

---
+++ examples/regression_california/example_elasticnet_california.py
@@ -0,0 +1,94 @@
+# %% ElasticNet, GridSearch, no argument
+import parent_import
+from tune_easy import ElasticNetTuning
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import numpy as np
+# Load dataset
+TARGET_VARIABLE = 'price'  # Target variable
+USE_EXPLANATORY = ['MedInc', 'AveOccup', 'Latitude', 'HouseAge']  # Explanatory variables
+california_housing = pd.DataFrame(np.column_stack((fetch_california_housing().data, fetch_california_housing().target)),
+        columns = np.append(fetch_california_housing().feature_names, TARGET_VARIABLE))
+california_housing = california_housing.sample(n=1000, random_state=42)  # sampling from 20640 to 1000
+y = california_housing[TARGET_VARIABLE].values
+X = california_housing[USE_EXPLANATORY].values
+# Run parameter tuning
+tuning = ElasticNetTuning(X, y, USE_EXPLANATORY, y_colname=TARGET_VARIABLE)
+tuning.plot_first_validation_curve()
+tuning.grid_search_tuning()
+tuning.plot_search_history()
+tuning.plot_search_map()
+tuning.plot_best_validation_curve()
+tuning.plot_best_learning_curve()
+tuning.plot_param_importances()
+
+# %% ElasticNet, RandomSearch, no argument
+import parent_import
+from tune_easy import ElasticNetTuning
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import numpy as np
+# Load dataset
+TARGET_VARIABLE = 'price'  # Target variable
+USE_EXPLANATORY = ['MedInc', 'AveOccup', 'Latitude', 'HouseAge']  # Explanatory variables
+california_housing = pd.DataFrame(np.column_stack((fetch_california_housing().data, fetch_california_housing().target)),
+        columns = np.append(fetch_california_housing().feature_names, TARGET_VARIABLE))
+california_housing = california_housing.sample(n=1000, random_state=42)  # sampling from 20640 to 1000
+y = california_housing[TARGET_VARIABLE].values
+X = california_housing[USE_EXPLANATORY].values
+# Run parameter tuning
+tuning = ElasticNetTuning(X, y, USE_EXPLANATORY, y_colname=TARGET_VARIABLE)
+tuning.random_search_tuning()
+tuning.plot_search_history()
+tuning.plot_search_map()
+tuning.plot_best_learning_curve()
+tuning.plot_best_validation_curve()
+tuning.plot_param_importances()
+
+# %% ElasticNet, BayesianOptimization, no argument
+import parent_import
+from tune_easy import ElasticNetTuning
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import numpy as np
+# Load dataset
+TARGET_VARIABLE = 'price'  # Target variable
+USE_EXPLANATORY = ['MedInc', 'AveOccup', 'Latitude', 'HouseAge']  # Explanatory variables
+california_housing = pd.DataFrame(np.column_stack((fetch_california_housing().data, fetch_california_housing().target)),
+        columns = np.append(fetch_california_housing().feature_names, TARGET_VARIABLE))
+california_housing = california_housing.sample(n=1000, random_state=42)  # sampling from 20640 to 1000
+y = california_housing[TARGET_VARIABLE].values
+X = california_housing[USE_EXPLANATORY].values
+# Run parameter tuning
+tuning = ElasticNetTuning(X, y, USE_EXPLANATORY, y_colname=TARGET_VARIABLE)
+tuning.bayes_opt_tuning()
+tuning.plot_search_history()
+tuning.plot_search_map()
+tuning.plot_best_learning_curve()
+tuning.plot_best_validation_curve()
+tuning.plot_param_importances()
+
+# %% ElasticNet, Optuna, no argument
+import parent_import
+from tune_easy import ElasticNetTuning
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import numpy as np
+# Load dataset
+TARGET_VARIABLE = 'price'  # Target variable
+USE_EXPLANATORY = ['MedInc', 'AveOccup', 'Latitude', 'HouseAge']  # Explanatory variables
+california_housing = pd.DataFrame(np.column_stack((fetch_california_housing().data, fetch_california_housing().target)),
+        columns = np.append(fetch_california_housing().feature_names, TARGET_VARIABLE))
+california_housing = california_housing.sample(n=1000, random_state=42)  # sampling from 20640 to 1000
+y = california_housing[TARGET_VARIABLE].values
+X = california_housing[USE_EXPLANATORY].values
+# Run parameter tuning
+tuning = ElasticNetTuning(X, y, USE_EXPLANATORY, y_colname=TARGET_VARIABLE)
+tuning.optuna_tuning()
+tuning.plot_search_history()
+tuning.plot_search_map()
+tuning.plot_best_learning_curve()
+tuning.plot_best_validation_curve()
+tuning.plot_param_importances()
+
+# %%
